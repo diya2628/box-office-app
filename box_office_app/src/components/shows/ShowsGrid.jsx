41bde6251b91c41032483161c6aca19e365e9319
Diya <diya.102628@gmail.com>
2024-04-03 08:21:07 +0530

    dynamic routing wid id done

--- box_office_app/src/components/shows/ShowsGrid.jsx
+++ box_office_app/src/components/shows/ShowsGrid.jsx
@@ -5,6 +5,7 @@ const ShowsGrid = ({ shows }) => {
       {shows.map(data => (
         <ShowCard
           key={data.show.id}
+          id={data.show.id}
           name={data.show.name}
           image={
             data.show.image ? data.show.image.medium : '\no-image-found.png'
