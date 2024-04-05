db01ce8890127381a8dde396e94d33ef97d77a88
Diya <diya.102628@gmail.com>
2024-04-05 08:05:47 +0530

    added query in home page

--- box_office_app/src/pages/Home.jsx
+++ box_office_app/src/pages/Home.jsx
@@ -4,24 +4,39 @@ import { searchForShows, searchForPeople } from './../api/tvmaze';
 import SearchForm from '../components/SearchForm';
 import ShowsGrid from '../components/shows/ShowsGrid';
 import ActorsGrid from '../components/actors/ActorsGrid';
+import { useQuery } from '@tanstack/react-query';
 
 const Home = () => {
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
+  const [filter, setFilter] = useState(null);
+
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: ['search', filter],
+    queryFn: () =>
+      filter.searchOption === 'shows'
+        ? searchForShows(filter.q)
+        : searchForPeople(filter.q),
+    // ⬇️ disabled as long as the filter is empty
+    enabled: !!filter,
+    refetchOnWindowFocus: false,
+  });
 
   const onSearch = async ({ q, searchOption }) => {
-    try {
-      setApiDataError(null);
-      if (searchOption == 'shows') {
-        const result = await searchForShows(q);
-        setApiData(result);
-      } else {
-        const result = await searchForPeople(q);
-        setApiData(result);
-      }
-    } catch (error) {
-      setApiDataError(error);
-    }
+    setFilter({ q, searchOption });
+
+    // const [apiData, setApiData] = useState(null);
+    // const [apiDataError, setApiDataError] = useState(null);
+    // try {
+    //   setApiDataError(null);  //earlier used now using usequery
+    //   if (searchOption == 'shows') {
+    //     const result = await searchForShows(q);
+    //     setApiData(result);
+    //   } else {
+    //     const result = await searchForPeople(q);
+    //     setApiData(result);
+    //   }
+    // } catch (error) {
+    //   setApiDataError(error);
+    // }
 
     // console.log(apiData);
     //   const response = await fetch(
